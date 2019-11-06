f69d67e5eb4db26ac90ffc366ff83ce48846ae17
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-06 19:50:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,5 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   
   phrase = "It's-a me, Mario!"
@@ -7,4 +8,14 @@ end
 
 def toadstool
   puts status
+end
+
+def link 
+  link_says = "It's dangerous To Go Alone! Take This."
+  puts link_says
+end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+  puts phrase
 end
